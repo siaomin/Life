c24b7597157c2e3aa844fb9587812b6947d77585
W.M Cheng <jhengsiaomin@gmail.com>
2020-03-26 15:57:56 +0800

    剛建立專案

---
+++ Life/Life/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Life.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
